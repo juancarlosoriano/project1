878f1de500910c814fbe0ef03d076df4bbd69cfe
JC Soriano <jc@juancarlosoriano.ca>
2019-09-18 05:24:40 -0400

    Added general component structure

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const App = () => {
-  return <div>Hello World! This is App component.</div>;
-};
+const App = () => (
+  <main id="app">
+    <header id="description-header">
+      <h1>Can't Hurt Me</h1>
+      <span>David Goggins, Ex-Navy Seal</span>
+    </header>
+    <div id="summary">
+      <p>
+        Can't Hurt Me is a biography by David Goggins about his life growing up
+        from a little kid into his years serving in US Military as a Navy Seal.
+      </p>
+    </div>
+    <section id="table-of-contents">
+      <details>
+        <summary>Chapter One</summary>
+        <p>This is the description of Chapter One</p>
+      </details>
+    </section>
+  </main>
+);
 
 export default App;
